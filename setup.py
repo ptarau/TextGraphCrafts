fe7bdfb14e7346fdb63fc9d403b7f05c84fc66ae
Andrea Cortis <andrea.cortis@gmail.com>
2019-12-14 16:10:11 -0600

    trying

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     with open("README.md","r") as f:
         long_description = f.read()
 
-    version = "0.0.1"
+    version = "0.0.2_a"
     RELEASE_VERSION = True
 
     if not RELEASE_VERSION:
@@ -34,17 +34,17 @@ if __name__ == '__main__':
     def _post_install(setup):
         def _post_actions():
             print ('--- Post install ---')
-            import nltk
-            import platform
-            if platform.system()=='darwin':
-                try:
-                    import ssl
-                    _create_unverified_https_context = ssl._create_unverified_context
-                except AttributeError:
-                    pass
-                else:
-                    ssl._create_default_https_context = _create_unverified_https_context
-            nltk.download()
+            # import nltk
+            # import platform
+            # if platform.system()=='darwin':
+            #     try:
+            #         import ssl
+            #         _create_unverified_https_context = ssl._create_unverified_context
+            #     except AttributeError:
+            #         pass
+            #     else:
+            #         ssl._create_default_https_context = _create_unverified_https_context
+            # nltk.download()
 
         _post_actions()
         return setup
@@ -54,6 +54,7 @@ if __name__ == '__main__':
           version=version,
           description='DeepRank Model: a deep learning approach to relevance ranking in information retrieval ',
           long_description = long_description,
+          long_description_content_type='text/markdown',
           url='https://github.com/ptarau/TextGraphCrafts.git',
           author='Paul Tarau, Andrea Cortis',
           author_USER_EMAIL='<paul.tarau@gmail.com>; andrea.cortis@gmail.com>',
